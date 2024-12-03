90e3658000be90cd833ac5d2d23681bf33c2324c
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-12-04 08:31:41 +1100

    Fix PackRepositoryExtension not being present on PackRepository.

---
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/PackRepositoryMixin.java
@@ -0,0 +1,25 @@
+package io.github.fabricators_of_create.porting_lib.mixin.client;
+
+import com.google.common.collect.ImmutableSet;
+import io.github.fabricators_of_create.porting_lib.extensions.PackRepositoryExtension;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.repository.RepositorySource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mixin(PackRepository.class)
+public class PackRepositoryMixin implements PackRepositoryExtension {
+    @Mutable
+    @Shadow @Final private Set<RepositorySource> sources;
+
+    public void pl$addPackFinder(RepositorySource packFinder) {
+        if (sources instanceof ImmutableSet<RepositorySource>)
+            sources = new HashSet<>(sources);
+        sources.add(packFinder);
+    }
+}
